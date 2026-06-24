0a6a565a147a00293d0156efd6507beb2b6920ed
Alexandre Avanci <120659030+AlexandreAvanci@users.noreply.github.com>
2026-06-23 21:47:22 -0300

    Update query_18.sql

--- queries/query_18.sql
+++ queries/query_18.sql
@@ -1,1 +1,9 @@
+18. Raising revenue by month.
 
+select
+sum(valor_da_consulta) as valor_total_por_mes,
+extract(month from data_consulta) as mes,
+extract(year from data_consulta) as ano
+from consultas
+group by mes, ano
+order by mes, ano asc;
