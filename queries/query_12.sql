ae02dba26d5d448c835e5bf7f9628883ff36de93
Alexandre Avanci <120659030+AlexandreAvanci@users.noreply.github.com>
2026-06-23 21:44:26 -0300

    Update query_12.sql

--- queries/query_12.sql
+++ queries/query_12.sql
@@ -1,1 +1,8 @@
+12. Which patient spent more on appointments?
 
+select nome, sum(valor_da_consulta) as paciente_que_gastou_mais from pacientes
+inner join consultas
+on pacientes.id = consultas.paciente_id 
+group by nome
+order by paciente_que_gastou_mais desc
+limit 1;
