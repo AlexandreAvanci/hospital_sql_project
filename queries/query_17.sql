e021230c8c683d98ac88d15e947115187f737b96
Alexandre Avanci <120659030+AlexandreAvanci@users.noreply.github.com>
2026-06-23 21:46:57 -0300

    Update query_17.sql

--- queries/query_17.sql
+++ queries/query_17.sql
@@ -1,1 +1,7 @@
+17. Revenue by doctor's specialty.
 
+select especialidade, sum(valor_da_consulta) as valor_total_por_especialidade from medicos
+inner join consultas
+on medicos.id = consultas.medico_id
+group by especialidade
+order by valor_total_por_especialidade desc;
