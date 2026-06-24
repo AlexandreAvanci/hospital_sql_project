295b94f878212cb491c0a525d223d0835112d57c
Alexandre Avanci <120659030+AlexandreAvanci@users.noreply.github.com>
2026-06-23 21:45:46 -0300

    Update query_16.sql

--- queries/query_16.sql
+++ queries/query_16.sql
@@ -1,1 +1,7 @@
+16. Revenue by city.
 
+select cidade, sum(valor_da_consulta) as faturamento_por_cidade from pacientes
+inner join consultas
+on pacientes.id = consultas.paciente_id
+group by cidade
+order by cidade asc;
