1f21481947288a1aaa4d77cf78ae82e6c7dfeee7
Alexandre Avanci <120659030+AlexandreAvanci@users.noreply.github.com>
2026-06-23 21:43:24 -0300

    Update query_08.sql

--- queries/query_08.sql
+++ queries/query_08.sql
@@ -1,1 +1,6 @@
+8. Show how many appointments each patient did.
 
+select nome, count(*) as quantidade_de_consultas from pacientes
+inner join consultas
+on pacientes.id = consultas.paciente_id
+group by nome;
