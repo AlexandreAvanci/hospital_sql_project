07c85887ca9473f243a20552d12ac2ea01ba1746
Alexandre Avanci <120659030+AlexandreAvanci@users.noreply.github.com>
2026-06-23 21:44:42 -0300

    Update query_13.sql

--- queries/query_13.sql
+++ queries/query_13.sql
@@ -1,1 +1,7 @@
+13. Appointments in last 30 days.
 
+select nome, data_consulta from pacientes
+inner join consultas
+on pacientes.id = consultas.paciente_id
+where data_consulta >= current_date - 30
+order by data_consulta desc;
