236eef45ac9ed2a9588a9e94d035ba3d1e1156fe
Alexandre Avanci <120659030+AlexandreAvanci@users.noreply.github.com>
2026-06-23 21:37:49 -0300

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -1,20 +1,23 @@
 # Insights/answers
 
-1.
-2.
-3.
-4.
-5.
-6.
-7.
-8.
-9.
-10.
-11.
-12.
-13.
-14.
-15.
-16.
-17.
-18.
+-- To consider: This analysis were made in the period between 2026-01-01 and 2026-06-30.
+  
+-- 1. The hospital have 50 patients registered in the hospital system.
+-- 2. The hospital have 20 doctors registered in the hospital system.
+-- 3. The hospital had in total 600 appointments between 2026-01-01 and 2026-06-30.
+-- 4. **Nothing to consider**
+-- 5. **Nothing to consider**
+-- 6. **Nothing to consider**
+-- 7. **Nothing to consider**
+-- 8. Letícia Ramos and Bruno Silva went to 20 appointments, both with more appointments between all the patients, while Juliana Ferreira went to only 05 appointments.
+-- 9. Ana Beatriz did 39 appointments as a doctor, the doctor who made more appointments between all the doctors from the hospital, while Carlos Henrique did only 22.
+-- 10. The raised for the bussiness with all appointments included was R$253.947,18 reais.
+-- 11. Juliana Rocha raised more from appointments. She raised R$17.300,76 reais,
+-- and Carlos Henrique less than the others from the appointments as a doctor. He raised R$8.188.68.
+-- 12. Leticia Ramos spent more than everyone. She spent R$8.517,26 on appointments, and Juliana Ferreira, the lowest one, spent R$2.087,45.
+-- 13. 135 appointments where made it in the last 30 days.
+-- 14. 106 appointments in January.
+-- 15. 94 appointments in May.
+-- 16. Curitiba raised R$69.631,49 in appointments, the city that raised more than the others. The last one, São Paulo, with R$47.798,42.
+-- 17. February was the worst month in revenue, with R$34.724,10. The month which had the better revenue was march, with R$45.551,16.
+-- 18. Carlos Henrique had the lowest medium ticket between all the doctors, with only R$372,21. Who had the highest medium ticket was the Felipe Costa, with R$468,57.
