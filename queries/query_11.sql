076d6e17a99a22ac8bc21f975af1acb540e0ea70
Alexandre Avanci <120659030+AlexandreAvanci@users.noreply.github.com>
2026-06-23 21:44:11 -0300

    Update query_11.sql

--- queries/query_11.sql
+++ queries/query_11.sql
@@ -1,1 +1,8 @@
+11. Which doctor raised more from the hospital?
 
+select nome, sum(valor_da_consulta) as valor_total_por_medico from medicos
+inner join consultas
+on medicos.id = consultas.medico_id
+group by nome
+order by valor_total_por_medico desc
+limit 1;
