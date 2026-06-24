d69f72925fe35c7eea583c78f3392b577134e3b1
Alexandre Avanci <120659030+AlexandreAvanci@users.noreply.github.com>
2026-06-23 21:43:42 -0300

    Update query_09.sql

--- queries/query_09.sql
+++ queries/query_09.sql
@@ -1,1 +1,6 @@
+9. Show how many appointments each doctor did.
 
+select nome, count (medico_id) as quantidade_de_consultas_realizadas from medicos
+inner join consultas
+on medicos.id = consultas.medico_id
+group by nome;
